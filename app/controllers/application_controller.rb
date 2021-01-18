ac4d1629d2f201a194a9876039c26f243e3a4488
builtbyjosh <admin@builtbyjosh.com>
2021-01-17 21:58:13 -0600

    login helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,8 @@ class ApplicationController < ActionController::Base
     def logged_in?
       !!current_user
     end
+
+    def require_login
+      redirect_to root_path unless logged_in?
+    end
 end
