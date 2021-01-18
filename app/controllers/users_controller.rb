b6f01694e7c7c5006f499824cff594f242ed4a79
builtbyjosh <admin@builtbyjosh.com>
2021-01-17 23:34:58 -0600

    refactor user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
-    before_action :require_login
+    
 
     def show
-        if current_user.id == params[:id].to_i
-            current_user.id 
+        if logged_in?
+            redirect_to user_path(current_user) unless current_user.id == params[:id].to_i
         else
-            flash[:errors] = "You can only view your profile"
-            redirect_to user_path(current_user.id)
-        end
+            require_login
+        end        
     end
     
 end
