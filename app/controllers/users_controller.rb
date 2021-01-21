ae567ec8fc3d109889e73a5fcbed8c2a997ecec9
builtbyjosh <admin@builtbyjosh.com>
2021-01-20 19:37:57 -0600

    remove line_item viwes, add edit user profile view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,20 @@ class UsersController < ApplicationController
             require_login
         end        
     end
+
+    def edit
+
+    end
+
+    def update
+        current_user.update(update_params)
+        redirect_to user_path(current_user)
+    end
+
+    private
+
+    def update_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
     
 end
