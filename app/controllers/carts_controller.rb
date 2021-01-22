0b409804e310127eea74496a0607eca40ea4a795
builtbyjosh <admin@builtbyjosh.com>
2021-01-22 15:23:07 -0600

    fix cart show routing bug

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,12 @@
 class CartsController < ApplicationController    
 
     def show
-        require_login unless logged_in?
-        # if logged_in?            
-        #     redirect_to user_cart_path(current_user, current_cart) unless current_cart.id == params[:id].to_i && current_user.id == params[:user_id].to_i            
-        # else
-        #     require_login
-        # end  
+        #require_login unless logged_in?
+        if logged_in?            
+            redirect_to user_cart_path(current_user, current_cart) unless current_cart.id == params[:id].to_i && current_user.id == params[:user_id].to_i            
+        else
+            require_login
+        end  
     end
 
     private
