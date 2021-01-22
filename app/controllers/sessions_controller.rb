7bbf98d3f96fce400e877d2b5a473bd3f0c66c7b
builtbyjosh <admin@builtbyjosh.com>
2021-01-22 15:16:45 -0600

    refactor error handling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,46 +1,43 @@
 class SessionsController < ApplicationController
-    def signup
-      new_user
+  def signup
+    new_user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save      
+      set_user(@user)
+      redirect_to user_path(@user)
+    else      
+      render :signup
     end
-  
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        flash[:notice] = "success"
-        set_user(@user)
-        redirect_to user_path(@user)
-      else
-        @error = "Please try again."
-        render :signup
-      end
-    end
-  
-    def login
-      new_user
-    end  
+  end
+
+  def login
+    new_user
+  end  
 
   def success
     if request.env["omniauth.auth"]  
       @user = User.find_by(uid: request.env["omniauth.auth"]["uid"])
       if @user.nil?
-          @user = User.create(username: request.env["omniauth.auth"]["info"]["nickname"], uid: request.env["omniauth.auth"]["uid"], password: "github")
+        @user = User.create(username: request.env["omniauth.auth"]["info"]["nickname"], uid: request.env["omniauth.auth"]["uid"], password: "github")
       end
       set_user(@user)
       redirect_to user_path(@user)
     else
       @user = User.find_by(username: params[:user][:username])        
-      if !@user
-        binding.pry
-          @error = "Account not found. Please try again."
-          new_user
-          redirect_to login_path
+      if !@user        
+        @error = "Account not found. Please try again."
+        new_user
+        redirect_to login_path
       elsif !@user.authenticate(params[:user][:password])          
-          @error = "Password incorrect. Please try again."
-          new_user
-          redirect_to login_path
+        @error = "Password incorrect. Please try again."
+        new_user
+        redirect_to login_path
       else          
-          set_user(@user)
-          redirect_to user_path(current_user.id)
+        set_user(@user)
+        redirect_to user_path(current_user.id)
       end
     end
   end
