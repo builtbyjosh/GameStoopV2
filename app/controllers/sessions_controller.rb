ceb99cd51fe52285fd93d69dab6e8861bd2b3cdf
builtbyjosh <admin@builtbyjosh.com>
2021-01-21 23:00:45 -0600

    refactor error messaging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,77 +10,63 @@ class SessionsController < ApplicationController
         set_user(@user)
         redirect_to user_path(@user)
       else
-        flash[:errors] = @user.errors.full_messages
+        @error = "Please try again."
         render :signup
       end
     end
   
     def login
       new_user
-    end
-  
-    # def success
-    #   binding.pry
-    #   @user = User.find_by(email: params[:user][:email])
-    #   if @user && @user.authenticate(user_params[:password])
-    #     set_user(@user)
-    #     current_cart
-    #     redirect_to user_path(@user)
-    #   else
-    #     flash[:errors] = "Invalid Email or Password"
-    #     new_user
-    #     render :login
-    #   end
-    # end
+    end  
 
-    def success
-      if request.env["omniauth.auth"]  
-        @user = User.find_by(uid: request.env["omniauth.auth"]["uid"])
-        if @user.nil?
-            @user = User.create(username: request.env["omniauth.auth"]["info"]["nickname"], uid: request.env["omniauth.auth"]["uid"], password: "github")
-        end
-        set_user(@user)
-        redirect_to user_path(@user)
+  def success
+    if request.env["omniauth.auth"]  
+      @user = User.find_by(uid: request.env["omniauth.auth"]["uid"])
+      if @user.nil?
+          @user = User.create(username: request.env["omniauth.auth"]["info"]["nickname"], uid: request.env["omniauth.auth"]["uid"], password: "github")
+      end
+      set_user(@user)
+      redirect_to user_path(@user)
     else
-        @user = User.find_by(username: params[:user][:username])        
-        if !@user
-          binding.pry
-            @error = "Account not found. Please try again."
-            new_user
-            redirect_to login_path
-        elsif !@user.authenticate(params[:user][:password])          
-            @error = "Password incorrect. Please try again."
-            new_user
-            redirect_to login_path
-        else          
-            set_user(@user)
-            redirect_to user_path(current_user.id)
-        end
+      @user = User.find_by(username: params[:user][:username])        
+      if !@user
+        binding.pry
+          @error = "Account not found. Please try again."
+          new_user
+          redirect_to login_path
+      elsif !@user.authenticate(params[:user][:password])          
+          @error = "Password incorrect. Please try again."
+          new_user
+          redirect_to login_path
+      else          
+          set_user(@user)
+          redirect_to user_path(current_user.id)
+      end
     end
   end
   
-    def logout
-      session.clear
-      redirect_to root_path
-    end
+  def logout
+    session.clear
+    redirect_to root_path
+  end
 
-    def checkout
-      @cart = current_cart
-      session.delete(:cart_id)
-    end
+  def checkout
+    @cart = current_cart
+    session.delete(:cart_id)
+  end
   
   
-    private
-  
-    def user_params
-        params.require(:user).permit(:username, :email, :password, :password_confirmation)
-    end
+  private
 
-    def new_user
-        @user = User.new
-    end
+  def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 
-    def set_user(user)
-      session[:user_id] = user.id
-    end
+  def new_user
+    @user = User.new
+  end
+
+  def set_user(user)
+    session[:user_id] = user.id
+  end
 end
