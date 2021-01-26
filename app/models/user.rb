203627ee9097154fa36230ee6e697c26238a327b
builtbyjosh <pc.refuge@gmail.com>
2021-01-26 13:51:43 -0600

    add email validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :carts
     has_many :line_items, through: :carts
-    validates :username, :password,  presence: true
-    validates :username, uniqueness: true
+    validates :username, :password, :email,  presence: true
+    validates :username, :email,  uniqueness: true
 
 end
